941838dcbfd1b1a73459ff13c933393189015911
julysNICK <julystube@gmail.com>
2023-01-12 18:23:28 -0300

    insert styles in single page

--- src/pages/DetailsCar/index.tsx
+++ src/pages/DetailsCar/index.tsx
@@ -1,6 +1,7 @@
 import * as styled from './styles';
 import { Header } from '../../components/Header';
 import { useState } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
 export default function DetailsCar() {
   const [currentImage, setCurrentImage] = useState(0);
@@ -50,9 +51,53 @@ export default function DetailsCar() {
             <styled.AmountOfImages>
               {currentImage + 1} / {images.length}
             </styled.AmountOfImages>
-            <styled.NextButton onClick={nextImage}></styled.NextButton>
-            <styled.PreviousButton onClick={prevImage}></styled.PreviousButton>
+            <styled.NextButton onClick={nextImage}>
+              <AiOutlineArrowRight size={30} color="#fff" />
+            </styled.NextButton>
+            <styled.PreviousButton onClick={prevImage}>
+              <AiOutlineArrowLeft size={30} color="#fff" />
+            </styled.PreviousButton>
           </styled.ContainerSlider>
+
+          <styled.ContainerInfoCar>
+            <styled.TitleCar>Ficha técnica</styled.TitleCar>
+            <styled.BoxInfo>
+              <styled.InfoTech>Marca:</styled.InfoTech>
+              <styled.InfoCar>Toyota</styled.InfoCar>
+            </styled.BoxInfo>
+            <styled.BoxInfo>
+              <styled.InfoTech>Modelo:</styled.InfoTech>
+              <styled.InfoCar>Corolla XEi 1.8 16V Flex 4p Aut.</styled.InfoCar>
+            </styled.BoxInfo>
+            <styled.BoxInfo>
+              <styled.InfoTech>Ano de compra:</styled.InfoTech>
+              <styled.InfoCar>2019</styled.InfoCar>
+            </styled.BoxInfo>
+
+            <styled.BoxInfo>
+              <styled.InfoTech>Motivo da venda:</styled.InfoTech>
+              <styled.InfoCar>
+                Estou vendendo pois preciso de um carro maior
+              </styled.InfoCar>
+            </styled.BoxInfo>
+
+            <styled.BoxInfo>
+              <styled.InfoTech>Proprietario:</styled.InfoTech>
+              <styled.InfoCar>João da Silva</styled.InfoCar>
+            </styled.BoxInfo>
+
+            <styled.BoxInfo>
+              <styled.InfoTech>Valor:</styled.InfoTech>
+              <styled.InfoCar>
+                <strong>R$ 50.000,00 à vista</strong>
+              </styled.InfoCar>
+            </styled.BoxInfo>
+          </styled.ContainerInfoCar>
+
+          <styled.BoxButtons>
+            <styled.ButtonGetInTouch>Entrar em contato</styled.ButtonGetInTouch>
+            <styled.ButtonMakeOffer>Fazer oferta</styled.ButtonMakeOffer>
+          </styled.BoxButtons>
         </styled.Container>
       </>
     </>
--- src/pages/DetailsCar/styles.ts
+++ src/pages/DetailsCar/styles.ts
@@ -23,7 +23,7 @@ export const AmountOfImages = styled.div`
   position: absolute;
   top: -2rem;
   right: 0;
-  background: #000;
+  background: #676464;
   color: #fff;
   padding: 0.5rem;
   font-size: 1.2rem;
@@ -35,11 +35,13 @@ export const NextButton = styled.button`
   top: 50%;
   right: 0;
   transform: translateY(-50%);
-  background: #000;
+  background: #807e7e;
   color: #fff;
   padding: 0.5rem;
   font-size: 1.2rem;
   font-weight: 500;
+  border: none;
+  border-radius: 0.5rem 0 0 0.5rem;
 `;
 
 export const PreviousButton = styled.button`
@@ -47,9 +49,90 @@ export const PreviousButton = styled.button`
   top: 50%;
   left: 0;
   transform: translateY(-50%);
-  background: #000;
+  background: #807e7e;
   color: #fff;
   padding: 0.5rem;
   font-size: 1.2rem;
   font-weight: 500;
+  border: none;
+  border-radius: 0 0.5rem 0.5rem 0;
+`;
+
+export const ContainerInfoCar = styled.div`
+  width: 100%;
+  margin-top: 2rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 2rem;
+
+  padding-bottom: 5rem;
+`;
+export const TitleCar = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 500;
+  color: #000;
+`;
+
+export const BoxInfo = styled.div`
+  width: 40%;
+  margin-top: 2rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  border-bottom: 1px solid #000;
+`;
+
+export const InfoTech = styled.p`
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #000;
+  margin-bottom: 1rem;
+`;
+
+export const InfoCar = styled.p`
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: #000;
+  margin-left: 1rem;
+`;
+
+export const BoxButtons = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  position: fixed;
+
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #fff;
+`;
+
+export const ButtonGetInTouch = styled.button`
+  width: 35%;
+  height: 4rem;
+  background: #f7ca00;
+  color: #0f1111;
+  font-size: 1.2rem;
+  font-weight: 500;
+  border: none;
+  cursor: pointer;
+  border-radius: 2rem;
+`;
+
+export const ButtonMakeOffer = styled.button`
+  width: 35%;
+  height: 4rem;
+  background: #ffa41c;
+  color: #000;
+  font-size: 1.2rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 2rem;
+  cursor: pointer;
 `;
